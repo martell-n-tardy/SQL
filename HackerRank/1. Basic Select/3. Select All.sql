81785cf4ed58fac270ba52ba8c3c70041ea154b1
martell <69739726+martell-n-tardy@users.noreply.github.com>
2021-11-01 22:59:10 -0700

    Create 3. Select All.sql

---
+++ HackerRank/1. Basic Select/3. Select All.sql
@@ -0,0 +1,7 @@
+-- Github    : https://github.com/martell-n-tardy
+-- HR-Profile: https://www.hackerrank.com/mtardy888
+-- Challenge : https://www.hackerrank.com/challenges/select-all-sql/problem
+-- Max Score : 10
+
+
+SELECT * FROM CITY;
