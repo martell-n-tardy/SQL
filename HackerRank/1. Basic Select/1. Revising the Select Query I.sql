535370a2a8568619c6ba7459ccac15405f8c67f4
martell <69739726+martell-n-tardy@users.noreply.github.com>
2021-11-01 22:43:44 -0700

    Update 1. Revising the Select Query I.sql

--- HackerRank/1. Basic Select/1. Revising the Select Query I.sql
+++ HackerRank/1. Basic Select/1. Revising the Select Query I.sql
@@ -1,1 +1,11 @@
+-- Github    : https://github.com/martell-n-tardy
+-- HR-Profile: https://www.hackerrank.com/mtardy888
+-- Challenge : https://www.hackerrank.com/challenges/revising-the-select-query
+-- Max Score : 10
 
+ 
+SELECT * FROM CITY 
+WHERE 
+POPULATION > 100000  
+AND 
+COUNTRYCODE = "USA";
