90b812ec9da0b0a48199434c3cc5d190c1a498a6
Martell Tardy, M.S <69739726+martell-n-tardy@users.noreply.github.com>
2022-01-10 23:22:10 -0800

    Delete HackerRank directory

--- HackerRank/1. Basic Select/3. Select All.sql
+++
@@ -1,7 +0,0 @@
--- Github    : https://github.com/martell-n-tardy
--- HR-Profile: https://www.hackerrank.com/mtardy888
--- Challenge : https://www.hackerrank.com/challenges/select-all-sql/problem
--- Max Score : 10
-
-
-SELECT * FROM CITY;
--- HackerRank/README.md
+++
@@ -1,2 +0,0 @@
-# HackerRank: SQL Challenge Solutions
-The query language for these solutions was completed in MySQL.
